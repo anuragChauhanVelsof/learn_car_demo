5e0db38577555fd3947972bdeaeac5e146c5c16c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-29 10:35:09 +0000

    Updating to latest FlutterFlow output.

--- lib/components/signs_card_widget.dart
+++ lib/components/signs_card_widget.dart
@@ -75,87 +75,110 @@ class _SignsCardWidgetState extends State<SignsCardWidget>
       alignment: const AlignmentDirectional(0.0, -1.0),
       child: Padding(
         padding: const EdgeInsetsDirectional.fromSTEB(20.0, 0.0, 20.0, 20.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: FlutterFlowTheme.of(context).primaryBackground,
-            borderRadius: BorderRadius.circular(10.0),
-            border: Border.all(
-              color: FlutterFlowTheme.of(context).secondaryBackground,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(16.0, 20.0, 16.0, 20.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  width: 50.0,
-                  height: 50.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    shape: BoxShape.rectangle,
-                    border: Border.all(
-                      color: FlutterFlowTheme.of(context).primaryBackground,
-                      width: 2.0,
-                    ),
+        child: InkWell(
+          splashColor: Colors.transparent,
+          focusColor: Colors.transparent,
+          hoverColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () async {
+            if (widget.leadingIcon!) {
+              context.pushNamed(
+                'signs',
+                queryParameters: {
+                  'title': serializeParam(
+                    widget.title,
+                    ParamType.String,
                   ),
-                  child: Align(
-                    alignment: const AlignmentDirectional(0.0, 0.0),
-                    child: Hero(
-                      tag: widget.image!,
-                      transitionOnUserGestures: true,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(0.0),
-                        child: CachedNetworkImage(
-                          fadeInDuration: const Duration(milliseconds: 500),
-                          fadeOutDuration: const Duration(milliseconds: 500),
-                          imageUrl: widget.image!,
-                          width: 30.0,
-                          height: 30.0,
-                          fit: BoxFit.contain,
-                          alignment: const Alignment(0.0, 0.0),
+                  'id': serializeParam(
+                    widget.id,
+                    ParamType.int,
+                  ),
+                }.withoutNulls,
+              );
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: FlutterFlowTheme.of(context).primaryBackground,
+              borderRadius: BorderRadius.circular(10.0),
+              border: Border.all(
+                color: FlutterFlowTheme.of(context).secondaryBackground,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsetsDirectional.fromSTEB(16.0, 20.0, 16.0, 20.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      shape: BoxShape.rectangle,
+                      border: Border.all(
+                        color: FlutterFlowTheme.of(context).primaryBackground,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: const AlignmentDirectional(0.0, 0.0),
+                      child: Hero(
+                        tag: widget.image!,
+                        transitionOnUserGestures: true,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(0.0),
+                          child: CachedNetworkImage(
+                            fadeInDuration: const Duration(milliseconds: 500),
+                            fadeOutDuration: const Duration(milliseconds: 500),
+                            imageUrl: widget.image!,
+                            width: 30.0,
+                            height: 30.0,
+                            fit: BoxFit.contain,
+                            alignment: const Alignment(0.0, 0.0),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding:
-                        const EdgeInsetsDirectional.fromSTEB(12.0, 0.0, 0.0, 0.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 4.0),
-                          child: Text(
-                            widget.title!,
-                            style: FlutterFlowTheme.of(context)
-                                .bodyLarge
-                                .override(
-                                  fontFamily: 'Poppins',
-                                  color:
-                                      FlutterFlowTheme.of(context).primaryText,
-                                  letterSpacing: 0.0,
-                                  fontWeight: FontWeight.w500,
-                                ),
+                  Expanded(
+                    child: Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(12.0, 0.0, 0.0, 0.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsetsDirectional.fromSTEB(
+                                0.0, 0.0, 0.0, 4.0),
+                            child: Text(
+                              widget.title!,
+                              style: FlutterFlowTheme.of(context)
+                                  .bodyLarge
+                                  .override(
+                                    fontFamily: 'Poppins',
+                                    color: FlutterFlowTheme.of(context)
+                                        .primaryText,
+                                    letterSpacing: 0.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                if (widget.leadingIcon ?? true)
-                  Icon(
-                    Icons.chevron_right_rounded,
-                    color: FlutterFlowTheme.of(context).secondaryText,
-                    size: 24.0,
-                  ),
-              ],
+                  if (widget.leadingIcon ?? true)
+                    Icon(
+                      Icons.chevron_right_rounded,
+                      color: FlutterFlowTheme.of(context).secondaryText,
+                      size: 24.0,
+                    ),
+                ],
+              ),
             ),
           ),
         ).animateOnPageLoad(animationsMap['containerOnPageLoadAnimation']!),
